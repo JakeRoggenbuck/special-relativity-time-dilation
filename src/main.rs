4935a10b35086ce23d29b12275918cb15e8d3f50
Jake <jakeroggenbuck2@gmail.com>
2024-08-27 01:32:46 -0700

    Add derivative

--- src/main.rs
+++ src/main.rs
@@ -6,7 +6,14 @@ fn time_dilation_by_velocity(t: f64, v: f64) -> f64 {
     t / (1.0 - (v * v) / (C * C)).sqrt()
 }
 
+fn derivative_time_dilation_by_velocity(t: f64, v: f64) -> f64 {
+    t * (-1.0 / 2.0 * (1.0 - ((v * v) * (C * C))).powf(-3.0 / 2.0) * -(2.0 * v) / (C * C))
+}
+
 fn main() {
     let a: f64 = time_dilation_by_velocity(60.0 * 60.0, 300.0);
     println!("{}", a);
+
+    let b: f64 = derivative_time_dilation_by_velocity(60.0 * 60.0, 300.0);
+    println!("{}", b);
 }
