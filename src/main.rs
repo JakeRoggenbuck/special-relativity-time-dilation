f3fc510aa32fe172dfee4e7f362326979f13090b
Jake <jakeroggenbuck2@gmail.com>
2024-09-30 09:27:59 -0700

    Add more prints

--- src/main.rs
+++ src/main.rs
@@ -3,7 +3,7 @@ const C: f64 = 299_792_458.0;
 /// t: time in seconds
 /// v: km/s
 fn time_dilation_by_velocity(t: f64, v: f64) -> f64 {
-    t / (1.0 - (v * v) / (C * C)).sqrt()
+    t / (1.0 - ((v * v) / (C * C))).sqrt()
 }
 
 fn derivative_time_dilation_by_velocity(t: f64, v: f64) -> f64 {
@@ -11,7 +11,13 @@ fn derivative_time_dilation_by_velocity(t: f64, v: f64) -> f64 {
 }
 
 fn main() {
-    let a: f64 = time_dilation_by_velocity(60.0 * 60.0, 300.0);
+    let a: f64 = time_dilation_by_velocity(25.0, 300.0);
+    println!("{}", a);
+
+    let a: f64 = time_dilation_by_velocity(25.0, 3000000.0);
+    println!("{}", a);
+
+    let a: f64 = time_dilation_by_velocity(25.0, 240000.0);
     println!("{}", a);
 
     let b: f64 = derivative_time_dilation_by_velocity(60.0 * 60.0, 300.0);
